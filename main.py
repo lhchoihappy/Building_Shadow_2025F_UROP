1022e67e9beb0d6c34007af1a67afaf680323936
HappyC11O3 <happpys374@gmail.com>
2025-10-14 17:18:35 +0800

    add one more hour

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ if __name__ == "__main__":
         base_path = os.path.splitext(geojson_path)[0]
         
         # ========================== Hour Specified Here ==========================
-        suntime = np.array([6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+        suntime = np.array([5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
         for hour in suntime:
 
         # Loop over 24 hours
